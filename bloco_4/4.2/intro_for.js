3ed056940324e1438b8549abe66ab3dbbbc4d606
Camila Dornas <camiladornas@gmail.com>
2021-05-25 09:19:44 -0300

    Add exercise for

--- bloco_4/4.2/intro_for.js
+++ bloco_4/4.2/intro_for.js
@@ -1,1 +1,13 @@
-teste
+let cars = ['Saab', 'Volvo', 'BMW'];
+//Posição ->  0   ,    1   ,   2   ;
+
+for (let index = 0; index < cars.length; index +=1){
+    console.log(cars[index])
+}
+// let index = 0 ---> declaração da variável
+//{
+// index < cars.length ---> condição de repetição.|| index <= cars.length -1.
+// cars.length ---> Quantidade de elementos do array, esta sempre será 1 número maior que as posições do index.
+// último index = 2 | length = 3.
+//}
+// index +=1 ---> atualização da variável declarada. Adiciona o valor determinado, na variável declarada, antes de iniciar a próxima execução. 
