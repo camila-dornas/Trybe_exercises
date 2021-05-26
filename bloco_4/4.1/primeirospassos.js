97b882659a370e0d72c5a676f39e6f74d5f39dd5
Camila Dornas <camiladornas@gmail.com>
2021-05-26 13:27:32 -0300

    Add primeiros passos em js

---
+++ bloco_4/4.1/primeirospassos.js
@@ -0,0 +1,42 @@
+// let a = 2
+// let b = 10
+
+// console.log('Adição: ' + (a+b));
+
+// console.log('Subitração: ' + (a-b));
+
+// console.log('Multiplicação: ' + a*b);
+
+// console.log('Divisão: ' + a/b);
+
+// console.log('Módulo: '  + a%b);
+//Faça um programa que retorne o maior de dois números. 
+
+let a = 3
+let b = 1
+
+if (a<b) {
+    console.log(b)
+}
+else if (a>b) {
+    console.log(a)
+}
+else {
+    console.log('São iguais')
+};
+//Faça um programa que retorne o maior de três números.
+let a = 3
+let b = 10
+let c = 2
+
+if (a>b) {
+}
+else if (a>c) {
+    console.log(a)
+}
+else if (b<c<a) {
+    console.log(a)
+}
+else {
+    console.log('São iguais')
+};
