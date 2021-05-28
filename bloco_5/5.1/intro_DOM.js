17ea8fb59b2d8706967cc8e5569caea400d66d45
Camila Dornas <camiladornas@gmail.com>
2021-05-28 11:16:43 -0300

    Finaliza intro_DOM

--- bloco_5/5.1/intro_DOM.js
+++ bloco_5/5.1/intro_DOM.js
@@ -1,3 +1,18 @@
-document.getElementById("page-title")= 'Filmes favoritos da Camis'
-const paragraph = document.getElementById("paragraph");
-      paragraph.style.color = "red";
+//Título
+document.getElementById("pageTitle").innerText = 'Filmes favoritos da Camis';
+pageTitle.style.color = "pink";
+//1º parágrafo
+document.getElementById('paragraph').innerText = "Camis ama filmes de suspense com finais surpreendentes. Filmes de terror com bastante sangue também são adorados por Camis, mas filmes de espirito poucas vezes agradaram a ela, uma vez que são repetitivos e nada surpreendentes.";
+paragraph.style.color = "gray";
+//Subtítulo
+document.getElementById("subtitle").innerText = 'Filmes românticos favoritos';
+subtitle.style.color = "orange";
+//2º parágrafo
+document.getElementById('secondParagraph').innerText = 'Camis tem paixão por apenas um filme romântico: "Ps: I love you!". é um dos poucos filmes de românce que, relamente, "toma" a atenção da Camis.'
+secondParagraph.style.color = "blue";
+//Subtítulo2
+document.getElementById('subtitle2').innerText = "Filmes que Camila não assiste";
+subtitle2.style.color = "green"
+//3º parágrafo
+document.getElementById('thirdParagraph').innerText = 'Camila tem pavor em assistir filmes com animais se machucando ou que morrem durante o enredo. Então, Camis evita ao máximo filme de histórias de cachorros e de guerras onde o cavalo era utilizado, apesar de saber que é ficção, são sentimentos que ela prefere não acessar, uma vez que Camila "entra" no enredo do filme e sente as tristezas, angústias e medos dos personagens.'
+thirdParagraph.style.color = "red";
