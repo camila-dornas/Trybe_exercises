09c56b56dd2a39112833077c0e6db8537c3f5d76
Camila Dornas <camiladornas@gmail.com>
2021-05-29 10:53:41 -0300

    Add exercises1

---
+++ bloco_4/4.1/exercise1.js
@@ -0,0 +1,13 @@
+//Constantes não podem ser alteradas//
+const nome = 'Camila';
+const birthyCity = 'Santos';
+let birthYear = 1996;
+console.log(nome);
+ console.log(birthyCity);
+ console.log(birthYear);
+ //Variáveis podem ser alteradas//
+ birthYear = 2030;
+ console.log(nome);
+ console.log(birthyCity);
+ console.log(birthYear);
+ //  birthyCity = 'BH'; ---> OCORRERÁ UM ERRO
