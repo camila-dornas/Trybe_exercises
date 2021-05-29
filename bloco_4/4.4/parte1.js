498a8bcabd8a264e5e47ed020a01db6a1f920025
Camila Dornas <camiladornas@gmail.com>
2021-05-29 20:56:15 -0300

    Finaliza parte1

--- bloco_4/4.4/parte1.js
+++ bloco_4/4.4/parte1.js
@@ -18,20 +18,23 @@ for(let key in info) {
 }
 //Defina um 2º objeto com a mesma estrutura (as mesmas chaves) do 1º e os seguintes valores: 'Tio Patinhas','Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. 
 //Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves.
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal no quadrinhos do Paro DOnald',
+    recorrente: 'Sim'
+};
+let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim'
+};
 
-
-
-//----------> TENTATIVA <-----------//
-
-// let info2 = {
-//     personagem: 'Tio Patinhas',
-//     origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
-//     nota: 'O último MacPatinhas',
-//     recorrente: 'Sim'
-// };
-
-// for(let key in info) {
-//     console.log(info2[key2]);
-// };
-
-// console.log(info[key] + ', ' + info2[key2]);
+for(let key in info) {
+    if(info[key] === info.recorrente){
+    console.log('Ambos recorrentes');
+} else {
+    console.log(info[key] + ' e ' + info2[key]);
+}
+}
