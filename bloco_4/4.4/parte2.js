ff79800153a0c28ec89c5f57e6a711704691e747
Camila Dornas <camiladornas@gmail.com>
2021-05-29 21:32:00 -0300

    Add tentaiva parte2

--- bloco_4/4.4/parte2.js
+++ bloco_4/4.4/parte2.js
@@ -1,3 +1,10 @@
-function palindromo () {
-    if ()
-}
+// let palavra = 'arara';
+function palindromo(word) {
+    for (index in word) {
+        if (word[index] === word[(word.length-1) - index]) {
+            return true;
+        } else {
+            return false
+        }
+    }
+} console.log(palindromo('arara'))
