fd64f6a60ac96ed9f8336f99b467864650012c14
Camila Dornas <camiladornas@gmail.com>
2021-07-12 16:34:45 -0300

    Add conteudo bloco 9

---
+++ bloco_9/9.1/conteudo.js
@@ -0,0 +1,108 @@
+// ---> OPERAÇÕES ASSINCRONAS <--- //
+setTimeout(() => {
+    console.log('Comprar parafusos') 
+    console.log('Adicionar ao estoque') 
+  }, 2000); // Apesar de ser codado antes, só é executado após a prioridade.
+  
+  console.log('1 - Receber roda'); 
+  console.log('2 - Encaixar parafusos'); 
+  console.log('3 - Fixar roda no carro');
+
+  ////////
+
+  const assert = require('assert');
+
+const pushNumber = (list, number) => list.push(number);
+
+let numbers = [];
+
+pushNumber(numbers, 1);
+pushNumber(numbers, 2);
+pushNumber(numbers, 3);
+
+assert.deepStrictEqual(numbers, [1, 2, 3]);
+
+/////////////
+
+const pushNumber2 = (list, number) => list.push(number);
+
+let numbers2 = [];
+
+setTimeout(() => pushNumber2(numbers2, 1), 3000);
+pushNumber2(numbers2, 2);
+pushNumber2(numbers2, 3);
+// assert.deepStrictEqual(numbers, [1, 2, 3]); // essa validação falha
+setTimeout(() => assert.deepStrictEqual(numbers2, [2, 3, 1]), 3000);
+ // essa validação funciona
+
+ // ---> CALLBACKS <--- //
+//  1 - No código abaixo você tem a função getUser , que retorna uma pessoa qualquer. Complete a função getUser de forma que ela receba uma outra função como parâmetro para que possa realizar as operações abaixo sobre a pessoa retornada.
+// const assert = require('assert');
+
+const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
+const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+
+const getUser = (callback) => {
+  const userToReturn = {
+    firstName: "Ivan",
+    lastName: "Ivanovich",
+    nationality: "Russian"
+  };
+  return callback(userToReturn)
+};
+
+assert.strictEqual(getUser(userFullName), "Hello! My name is Ivan Ivanovich"); // complete a chamada da função de getUser
+assert.strictEqual(getUser(userNationality), "Ivan is Russian"); // complete a chamada da função de getUser
+
+// 2 - No código abaixo você tem a função getUser modificada, que agora funciona de modo assíncrono e imprime dados de uma pessoa qualquer depois de um certo tempo. Complete a função getUser de forma que ela receba um callback para que possa realizar as operações abaixo sobre a pessoa.
+const userFullName2 = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
+const userNationality2 = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+
+const delay = (maxMilliseconds = 5000) => Math.floor(Math.random() * maxMilliseconds);
+
+const getUser2 = (callback) => {
+  setTimeout(() => {
+    const user = {
+      firstName: "Ivan",
+      lastName: "Ivanovich",
+      nationality: "Russian",
+    };
+    console.log(callback(user));
+  }, delay());
+};
+
+getUser2(userFullName2); // deve imprimir "Hello! My name is Ivan Ivanovich" depois de um certo tempo
+getUser2(userNationality2); // deve imprimir "Ivan is Russian" depois de um certo tempo
+
+// ---> LIDANDO COM ERROS EM OPERAÇÕES ASSINCRONAS <--- //
+// 1 - A função getCountry abaixo tem aproximadamente 50% de chance de obter com sucesso um país, tendo um callback para poder ser feita qualquer operação sobre o país retornado. Adicione um outro callback para getCountry , de forma que trate a mensagem de erro retornada.
+const countryName = ({ name }) => console.log(`Returned country is ${name}`);
+const countryCurrency = ({ name, currency }) => console.log(`${name}'s currency is the ${currency}`);
+
+const delay = (maxMilliseconds = 5000) => Math.floor(Math.random() * maxMilliseconds);
+
+const printErrorMessage = (error) => console.log(`Error getting country: ${error}`);
+
+const getCountry = (onSuccess, onError) => {
+  setTimeout(() => {
+    const didOperationSucceed = Math.random() >= 0.5;
+    if(didOperationSucceed) {
+      const country = {
+        name: "Brazil",
+        hdi: 0.759,
+        currency: "Real",
+      };
+      onSuccess(country);
+    }
+    else {
+      const errorMessage = "Country could not be found";
+      onError(errorMessage);
+    }
+  }, delay());
+};
+
+// Deve imprimir "Returned country is Brazil" no sucesso, ou "Error getting country: Country could not be found" em falha
+getCountry(countryName, printErrorMessage);
+
+// Deve imprimir "Brazil's currency is the Real" no sucesso, ou "Error getting country: Country could not be found" em falha
+getCountry(countryCurrency, printErrorMessage);
