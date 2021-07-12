4ff312c44c3b7fe127545caff6c3d86f7aa9ff29
Camila Dornas <camiladornas@gmail.com>
2021-07-12 11:41:59 -0300

    Add bloco 8

--- bloco_8/8.2/exercicios.js
+++ bloco_8/8.2/exercicios.js
@@ -84,19 +84,67 @@ function smallerName() {
   assert.strictEqual(smallerName(), 'Duna');
 
 // 3 - Encontre o primeiro livro cujo nome possui 26 caracteres.
-const expectedResult = {
-    author: {
-      birthYear: 1948,
-      name: 'George R. R. Martin',
-    },
-    genre: 'Fantasia',
+const getNamedBook = books.find(((element) => element.name.length === 26)).name;
+
+  console.log(getNamedBook);
+
+  assert.deepStrictEqual(getNamedBook, 'As Crônicas de Gelo e Fogo',);
+
+  const booksOrderedByReleaseYearDesc = books.sort(((a,b) => b.releaseYear - a.releaseYear));
+console.log(booksOrderedByReleaseYearDesc);
+  
+
+  // 4 - Ordene os livros por data de lançamento em ordem decrescente.
+//   const expectedResult = [1991, 1986, 1965, 1954, 1951, 1928];
+
+const expectedResult = [
+  {
     id: 1,
     name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: { name: 'George R. R. Martin', birthYear: 1948 },
     releaseYear: 1991,
-};
- 
-// const getNamedBook = expectedResult.find(((element) => element.name.length === 26)).name;
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: { name: 'Stephen King', birthYear: 1947 },
+    releaseYear: 1986,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: { name: 'Frank Herbert', birthYear: 1920 },
+    releaseYear: 1965,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: { name: 'Isaac Asimov', birthYear: 1920 },
+    releaseYear: 1951,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: { name: 'H. P. Lovecraft', birthYear: 1890 },
+    releaseYear: 1928,
+  },
+];
 
-//   console.log(getNamedBook);
+const booksOrderedByReleaseYearDesc = books.sort(((a,b) => b.releaseYear - a.releaseYear));
+console.log(booksOrderedByReleaseYearDesc);
+  
+  assert.deepStrictEqual(booksOrderedByReleaseYearDesc, expectedResult);
 
-//   assert.deepStrictEqual(getNamedBook, expectedResult);
+  // 5 - Faça uma função que retorne true , se todas as pessoas autoras nasceram no século XX, ou false , caso contrário.
